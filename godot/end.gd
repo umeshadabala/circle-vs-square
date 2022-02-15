d1d13cb676983a1c1bf564f4b989e61e904978dc
umi <84436237+umeshlab@users.noreply.github.com>
2022-02-15 09:15:13 +0530

    Add files via upload

---
+++ godot/end.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://menu.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().change_scene("res://world.tscn")
+
+
+func _on_Button3_pressed():
+	get_tree().change_scene("res://options.tscn")
---
+++ godot/player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+var movespeed = 500
+var bullet_speed = 2000
+var bullet = preload("res://bullet.tscn")
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var motion = Vector2()
+	if Input.is_action_pressed("ui_up"):
+		motion.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		motion.y += 1
+	if Input.is_action_pressed("ui_left"):
+		motion.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		motion.x += 1
+	motion = motion.normalized()
+	motion = move_and_slide(motion * movespeed)
+	look_at(get_global_mouse_position())
+	if Input.is_action_pressed("Shoot"):
+		fire()
+func fire():
+	var bullet_instance = bullet.instance()
+	bullet_instance.position = get_global_mouse_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(),Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child", bullet_instance)
+
+
+func _on_Area2D_body_entered(body):
+	if "enemy" in body.name:
+		queue_free()
+		get_tree().change_scene("res://end.tscn")
